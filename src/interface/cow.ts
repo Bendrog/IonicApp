c4f424d63ef68b3dac7d524398ee3a267657438e
Bendrog <32641320+Bendrog@users.noreply.github.com>
2018-07-03 14:20:14 +0200

    Create cow.ts

---
+++ src/interface/cow.ts
@@ -0,0 +1,11 @@
+
+   export interface Cow {
+        id: string;
+        age: string;
+        name: string;
+        code_race: number;
+        date_de_terme: string;
+        sexee_femelle: boolean;
+        croisement: boolean;
+        reforme: boolean;
+    }
